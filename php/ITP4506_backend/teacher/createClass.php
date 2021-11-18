01219f6d9d8aad555c477c0204f5acdcedda9384
kjjkjjzyayufqza <kjjkjjzyayufqza@yahoo.com>
2021-11-18 17:55:40 +0800

    updata bug

--- php/ITP4506_backend/teacher/createClass.php
+++ php/ITP4506_backend/teacher/createClass.php
@@ -3,8 +3,8 @@ require_once('../conn.php');
 
 extract($_POST);
 $id = rand(100000000,219999999);
-$sql = "INSERT INTO class (id, name, description, teacherID, academicYear)
-    VALUES ($id, $name, $description, $teacherID, $academicYear)";
+$sql = "INSERT INTO class (id, name, description, teacherID, academicYear,archive)
+    VALUES ($id, '" . $name . "', '" . $description . "', '" . $teacherID . "', '" . $academicYear . "',1)";
 $rs = mysqli_query($conn, $sql);
-
+echo $sql;
 ?>
